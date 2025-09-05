eb89cc69912a02230e68e56710c426e089bde2bf
Rakshitha Kolimi <kolimi.rakshitha@intiqo.com>
2025-09-05 23:25:10 +0530

    CRUD APIs

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,10 +21,7 @@ type IExpenseInput struct {
 	Date     string `json:"date"`
 }
 
-var dummyData = []IExpense{
-	{ID: 1, Amount: 200, Category: "Food", Date: "2025-09-04"},
-	{ID: 2, Amount: 1500, Category: "Rent", Date: "2025-09-01"},
-}
+var expenses = make([]IExpense,0)
 
 func main() {
 	r := gin.Default()
@@ -31,7 +30,26 @@ func main() {
 	})
 
 	r.GET("/expenses", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, dummyData)
+		ctx.JSON(http.StatusOK, expenses)
+	})
+
+	r.GET("/expense/:id", func(ctx *gin.Context) {
+		var expense IExpense
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+
+		for _, exp := range expenses {
+			if exp.ID == id {
+				expense = exp
+				ctx.JSON(http.StatusOK, expense)
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusBadRequest, gin.H{"msg": fmt.Errorf("expense with ID %s doesnot exsist", strconv.Itoa(id))})
 	})
 
 	r.POST("/expense", func(ctx *gin.Context) {
@@ -42,15 +60,78 @@ func main() {
 			return
 		}
 
+		prevExpense := expenses[len(expenses)-1]
 		// add the expense
 		expense := IExpense{
-			ID:       len(dummyData) + 1,
+			ID:        prevExpense.ID + 1,
 			Amount:   in.Amount,
 			Category: in.Category,
 			Date:     in.Date,
 		}
-		dummyData = append(dummyData, expense)
+		expenses = append(expenses, expense)
 		ctx.JSON(http.StatusOK, expense)
 	})
+
+	r.PUT("/expense/:id", func(ctx *gin.Context) {
+		// Bind the input
+		var in IExpenseInput
+		if err := ctx.ShouldBindJSON(&in); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var expense IExpense
+		index := 0
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+
+		for i, exp := range expenses {
+			if exp.ID == id {
+				expense = exp
+				index = i + 1
+				break
+			}
+		}
+
+		if index == 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"msg": fmt.Errorf("expense with ID %s doesnot exsist", strconv.Itoa(id))})
+		}
+
+		// update the expense
+		expense.Amount = in.Amount
+		expense.Category = in.Category
+		expense.Date = in.Date
+
+		expenses[index-1] = expense
+		ctx.JSON(http.StatusOK, expense)
+	})
+
+	r.DELETE("/expense", func(ctx *gin.Context) {
+		// Bind the input
+		index := 0
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+
+		for i, exp := range expenses {
+			if exp.ID == id {
+				index = i + 1
+				break
+			}
+		}
+
+		if index == 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"msg": fmt.Errorf("expense with ID %s doesnot exsist", strconv.Itoa(id))})
+		}
+
+		expenses = append(expenses[:index-1], expenses[index+1:]...) 
+		ctx.JSON(http.StatusOK, "Expense is deleted successfully!")
+	})
+
 	http.ListenAndServe(":8080", r)
 }
